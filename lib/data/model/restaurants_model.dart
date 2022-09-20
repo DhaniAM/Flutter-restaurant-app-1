65dc498c3bf947cf2d8adbd806d2579f75f9f430
Dhani <dhani.a.mm@gmail.com>
2022-09-20 19:43:35 +0800

    changed restaurant data model from local json to dicoding api

---
+++ lib/data/model/restaurants_model.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+RestaurantsModel restaurantsDataFromJson(String str) =>
+    RestaurantsModel.fromJson(json.decode(str));
+
+class RestaurantsModel {
+  RestaurantsModel({
+    required this.error,
+    required this.message,
+    required this.count,
+    required this.restaurants,
+  });
+
+  bool error;
+  String message;
+  int count;
+  List<Restaurant> restaurants;
+
+  factory RestaurantsModel.fromJson(Map<String, dynamic> json) =>
+      RestaurantsModel(
+        error: json["error"],
+        message: json["message"],
+        count: json["count"],
+        restaurants: List<Restaurant>.from(
+            json["restaurants"].map((x) => Restaurant.fromJson(x))),
+      );
+}
+
+class Restaurant {
+  Restaurant({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.pictureId,
+    required this.city,
+    required this.rating,
+  });
+
+  String id;
+  String name;
+  String description;
+  String pictureId;
+  String city;
+  double rating;
+
+  factory Restaurant.fromJson(Map<String, dynamic> json) => Restaurant(
+        id: json["id"],
+        name: json["name"],
+        description: json["description"],
+        pictureId: json["pictureId"],
+        city: json["city"],
+        rating: json["rating"].toDouble(),
+      );
+}
