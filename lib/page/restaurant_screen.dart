f76888ddb8722c681f8345f7a38a2c1d733c69a5
Dhani <dhani.a.mm@gmail.com>
2022-09-20 22:51:57 +0800

    added loading screen when loading to restaurant_screen so no more null error, app works great!!!

--- lib/page/restaurant_screen.dart
+++ lib/page/restaurant_screen.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:restaurant_app_1/data/api/api_service.dart';
 import 'package:restaurant_app_1/data/model/restaurant_model.dart';
 import 'package:restaurant_app_1/widget/menu_item_name.dart';
-import 'package:restaurant_app_1/data/model/restaurants_model.dart';
 
 class RestaurantScreen extends StatelessWidget {
   final String restaurantId;
@@ -24,148 +23,164 @@ class RestaurantScreen extends StatelessWidget {
 
     return FutureBuilder(
       future: ApiService().getRestaurantDetail(restaurantId),
-      builder: (context, snapshot) {
-        RestaurantDetail resDetail = snapshot.data as RestaurantDetail;
-        final int resFoodsLen = resDetail.restaurant.menus.foods.length;
-        final int resDrinksLen = resDetail.restaurant.menus.drinks.length;
-        return Scaffold(
-          appBar: AppBar(
-            title: Text(resDetail.restaurant.name),
-            backgroundColor: const Color.fromRGBO(255, 106, 106, 1),
-          ),
-          body: SafeArea(
-            child: ListView(
-              children: <Widget>[
-                /// Top Image
-                Image.network((medImg + resDetail.restaurant.pictureId),
-                    loadingBuilder: (context, child, loadingProgress) {
-                  if (loadingProgress == null) return child;
-                  return const Center(child: CircularProgressIndicator());
-                }),
+      builder: (context, data) {
+        if (data.hasData) {
+          late RestaurantDetail resDetail = data.data as RestaurantDetail;
+          final int resFoodsLen = resDetail.restaurant.menus.foods.length;
+          final int resDrinksLen = resDetail.restaurant.menus.drinks.length;
+          return Scaffold(
+            appBar: AppBar(
+              title: Text(resDetail.restaurant.name),
+              backgroundColor: const Color.fromRGBO(255, 106, 106, 1),
+            ),
+            body: SafeArea(
+              child: ListView(
+                children: <Widget>[
+                  /// Top Image
+                  Image.network((medImg + resDetail.restaurant.pictureId),
+                      loadingBuilder: (context, child, loadingProgress) {
+                    if (loadingProgress == null) return child;
+                    return const Center(child: CircularProgressIndicator());
+                  }),
 
-                Container(
-                  padding: const EdgeInsets.all(16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Flexible(
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            /// Restaurant Name
-                            Text(
-                              resDetail.restaurant.name,
-                              style: const TextStyle(
-                                fontSize: 32,
-                                fontWeight: FontWeight.bold,
+                  Container(
+                    padding: const EdgeInsets.all(16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Flexible(
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              /// Restaurant Name
+                              Text(
+                                resDetail.restaurant.name,
+                                style: const TextStyle(
+                                  fontSize: 32,
+                                  fontWeight: FontWeight.bold,
+                                ),
                               ),
-                            ),
 
-                            /// Restaurant City
+                              /// Restaurant City
+                              Text(
+                                resDetail.restaurant.city,
+                                style: const TextStyle(fontSize: 18),
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        /// Restaurant rating
+                        Row(
+                          children: <Widget>[
+                            const Icon(
+                              Icons.star_rounded,
+                              color: Colors.yellow,
+                            ),
                             Text(
-                              resDetail.restaurant.city,
-                              style: const TextStyle(fontSize: 18),
+                              resDetail.restaurant.rating.toString(),
                             ),
                           ],
                         ),
-                      ),
-
-                      /// Restaurant rating
-                      Row(
-                        children: <Widget>[
-                          const Icon(
-                            Icons.star_rounded,
-                            color: Colors.yellow,
-                          ),
-                          Text(
-                            resDetail.restaurant.rating.toString(),
-                          ),
-                        ],
-                      ),
-                    ],
+                      ],
+                    ),
                   ),
-                ),
 
-                myDivider,
+                  myDivider,
 
-                /// Description
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    resDetail.restaurant.description,
-                    style: const TextStyle(fontSize: 15),
-                  ),
-                ),
-
-                myDivider,
-
-                /// Menu Title
-                const Center(
-                  child: Padding(
-                    padding: EdgeInsets.only(top: 16.0),
+                  /// Description
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
                     child: Text(
-                      "Menu",
+                      resDetail.restaurant.description,
+                      style: const TextStyle(fontSize: 15),
+                    ),
+                  ),
+
+                  myDivider,
+
+                  /// Menu Title
+                  const Center(
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 16.0),
+                      child: Text(
+                        "Menu",
+                        style: TextStyle(
+                          fontSize: 26,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  /// Foods Title
+                  const Padding(
+                    padding: EdgeInsets.only(left: 16),
+                    child: Text(
+                      "Food",
                       style: TextStyle(
-                        fontSize: 26,
+                        fontSize: 20,
                         fontWeight: FontWeight.bold,
                       ),
                     ),
                   ),
-                ),
 
-                /// Foods Title
-                const Padding(
-                  padding: EdgeInsets.only(left: 16),
-                  child: Text(
-                    "Food",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                  /// Foods Item
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Wrap(
+                      children: <Widget>[
+                        for (int i = 0; i < resFoodsLen; i++)
+                          MenuItemName(
+                              itemName:
+                                  resDetail.restaurant.menus.foods[i].name),
+                      ],
                     ),
                   ),
-                ),
 
-                /// Foods Item
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Wrap(
-                    children: <Widget>[
-                      for (int i = 0; i < resFoodsLen; i++)
-                        MenuItemName(
-                            itemName: resDetail.restaurant.menus.foods[i].name),
-                    ],
-                  ),
-                ),
-
-                /// Drinks Title
-                const Padding(
-                  padding: EdgeInsets.only(left: 16),
-                  child: Text(
-                    "Drink",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+                  /// Drinks Title
+                  const Padding(
+                    padding: EdgeInsets.only(left: 16),
+                    child: Text(
+                      "Drink",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
                     ),
                   ),
-                ),
 
-                /// Drinks Item
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Wrap(
-                    children: <Widget>[
-                      for (int i = 0; i < resDrinksLen; i++)
-                        MenuItemName(
-                            itemName:
-                                resDetail.restaurant.menus.drinks[i].name),
-                    ],
+                  /// Drinks Item
+                  Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Wrap(
+                      children: <Widget>[
+                        for (int i = 0; i < resDrinksLen; i++)
+                          MenuItemName(
+                              itemName:
+                                  resDetail.restaurant.menus.drinks[i].name),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        );
-      },
+          );
+        } else if (data.hasError) {
+          return const Scaffold(
+            body: Center(
+              child: Text('Error Loading Data -_-'),
+            ),
+          );
+        } else {
+          /// While Loading, show loading screen
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+      }, // builder
     );
   }
 }
