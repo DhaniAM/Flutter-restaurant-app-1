d009ed5362807a839a2ac423d864b23bd6959e00
Dhani <dhani.a.mm@gmail.com>
2022-09-24 18:08:24 +0800

    need to finish favorite_list_result

--- lib/widget/favorite_list_result.dart
+++ lib/widget/favorite_list_result.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:restaurant_app_1/data/provider/favorite_provier.dart';
+import 'package:restaurant_app_1/data/provider/home_provider.dart';
+import 'package:restaurant_app_1/widget/restaurant_card.dart';
 
 class FavoriteListResult extends StatelessWidget {
   const FavoriteListResult({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 3,
-      itemBuilder: (context, index) {
-        return Text('heloo');
+    return Consumer2<HomeProvider, FavoriteProvider>(
+      builder:
+          (context, HomeProvider homeData, FavoriteProvider favData, child) {
+        if (favData.favPrefs == true) {
+          return ListView.builder(itemCount: 2 ,itemBuilder: (context, index) {
+            return RestaurantCard(restaurants: Restaurants)
+          },));
+        }
       },
     );
   }
