16e470d55df2e8d3a22e72c068771d437a88ade7
Dhani <dhani.a.mm@gmail.com>
2022-09-26 00:08:43 +0800

    added notification_helper, background_service, data_time_helper

---
+++ lib/utils/background_service.dart
@@ -0,0 +1,36 @@
+import 'dart:ui';
+import 'dart:isolate';
+import 'package:restaurant_app_1/main.dart';
+import 'package:restaurant_app_1/data/api/api_service.dart';
+import 'package:restaurant_app_1/utils/notification_helper.dart';
+
+final ReceivePort port = ReceivePort();
+
+class BackgroundService {
+  static BackgroundService? _instance;
+  static const String _isolateName = 'isolate';
+  static SendPort? _uiSendPort;
+
+  BackgroundService._internal() {
+    _instance = this;
+  }
+
+  factory BackgroundService() => _instance ?? BackgroundService._internal();
+
+  void initializeIsolate() {
+    IsolateNameServer.registerPortWithName(
+      port.sendPort,
+      _isolateName,
+    );
+  }
+
+  static Future<void> callback() async {
+    print('Alarm fired!');
+    final NotificationHelper notificationHelper = NotificationHelper();
+    var result = await ApiService().getRestaurantsList();
+    await notificationHelper.showNotification(flutterLocalNotificationsPlugin, result);
+
+    _uiSendPort ??= IsolateNameServer.lookupPortByName(_isolateName);
+    _uiSendPort?.send(null);
+  }
+}
---
+++ lib/utils/notification_helper.dart
@@ -0,0 +1,80 @@
+import 'dart:convert';
+
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
+import 'package:restaurant_app_1/common/navigation.dart';
+import 'package:restaurant_app_1/data/model/restaurants_model.dart';
+import 'package:rxdart/subjects.dart';
+
+final selectNotificationSubject = BehaviorSubject<String>();
+
+class NotificationHelper {
+  static NotificationHelper? _instance;
+
+  NotificationHelper._internal() {
+    _instance = this;
+  }
+
+  factory NotificationHelper() => _instance ?? NotificationHelper._internal();
+
+  Future<void> initNotification(
+      FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin) async {
+    var initializationSettingsAndroid = const AndroidInitializationSettings('app_icon');
+
+    var initializationSettingsIOS = const DarwinInitializationSettings(
+      requestAlertPermission: false,
+      requestBadgePermission: false,
+      requestSoundPermission: false,
+    );
+
+    var initializationSettings = InitializationSettings(
+        android: initializationSettingsAndroid, iOS: initializationSettingsIOS);
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onDidReceiveNotificationResponse: (NotificationResponse details) async {
+        final payload = details.payload;
+        if (payload != null) {
+          print("notification payload: " + payload);
+        }
+        selectNotificationSubject.add(payload ?? "empty payload");
+      },
+    );
+  }
+
+  Future<void> showNotification(FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin,
+      RestaurantsList restaurantsList) async {
+    var channelId = "1";
+    var channelName = "channel_01";
+    var channelDescription = "Dhani Restaurant App";
+
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(channelId, channelName,
+        channelDescription: channelDescription,
+        importance: Importance.max,
+        priority: Priority.high,
+        ticker: 'ticker',
+        styleInformation: const DefaultStyleInformation(true, true));
+
+    var iOSPlatformChannelSpecifics = const DarwinNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        android: androidPlatformChannelSpecifics, iOS: iOSPlatformChannelSpecifics);
+
+    var titleNotification = "<b>Headline News</b>";
+
+    /// Restaurants title to show
+    var titleNews = restaurantsList.restaurants[0].name;
+
+    await flutterLocalNotificationsPlugin.show(
+        0, titleNotification, titleNews, platformChannelSpecifics,
+        payload: json.encode(restaurantsList.toJson()));
+  }
+
+  void configureSelectNotificationSubject(String route) {
+    selectNotificationSubject.stream.listen(
+      (String payload) async {
+        var data = RestaurantsList.fromJson(json.decode(payload));
+        var restaurant = data.restaurants[0];
+        Navigation.intentWithData(route, restaurant);
+      },
+    );
+  }
+}
