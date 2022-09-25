e3758acdce6189b5a629eab3f1c5cd08bf233a9a
Dhani <dhani.a.mm@gmail.com>
2022-09-26 00:21:19 +0800

    added custom_dialog.dart

---
+++ lib/widget/custom_dialog.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:restaurant_app_1/common/navigation.dart';
+
+@override
+customDialog(BuildContext context) {
+  if (Platform.isIOS) {
+    showCupertinoDialog(
+      context: context,
+      barrierDismissible: true,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('Coming Soon!'),
+          content: const Text('This feature will be coming soon!'),
+          actions: [
+            CupertinoDialogAction(
+              child: const Text('Ok'),
+              onPressed: () {
+                Navigation.back();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  } else {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text('Coming Soon!'),
+          content: const Text('This feature will be coming soon!'),
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigation.back();
+              },
+              child: const Text('Ok'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
