4f9914fd8581d7ba0a1587ff37eba68717e49ff8
Dhani <dhani.a.mm@gmail.com>
2022-09-26 01:12:25 +0800

    added some in main.dart and added main.dart to background_service.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
+import 'dart:io';
+
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:provider/provider.dart';
+import 'package:restaurant_app_1/common/navigation.dart';
 import 'package:restaurant_app_1/data/api/api_service.dart';
 import 'package:restaurant_app_1/data/db/database_helper.dart';
 import 'package:restaurant_app_1/data/provider/database_provider.dart';
@@ -10,8 +15,26 @@ import 'package:restaurant_app_1/data/provider/search_provider.dart';
 import 'package:restaurant_app_1/page/main_screen.dart';
 import 'package:restaurant_app_1/page/restaurant_screen.dart';
 import 'package:restaurant_app_1/page/search_screen.dart';
+import 'package:restaurant_app_1/utils/background_service.dart';
+import 'package:restaurant_app_1/utils/notification_helper.dart';
 
-void main() {
+final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+    FlutterLocalNotificationsPlugin();
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized;
+
+  final NotificationHelper _notificationHelper = NotificationHelper();
+  final BackgroundService _service = BackgroundService();
+
+  _service.initializeIsolate;
+
+  if (Platform.isAndroid) {
+    await AndroidAlarmManager.initialize();
+  }
+
+  await _notificationHelper.initNotification(flutterLocalNotificationsPlugin);
+
   runApp(const MyApp());
 }
 
@@ -47,6 +70,7 @@ class MyApp extends StatelessWidget {
           primaryColor: const Color.fromRGBO(255, 106, 106, 1),
           splashColor: const Color.fromRGBO(67, 218, 239, 1),
         ),
+        navigatorKey: navigatorKey,
         initialRoute: "/",
         routes: {
           "/": (context) => const MainScreen(),
